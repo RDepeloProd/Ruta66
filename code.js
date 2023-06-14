500216bca377b043c2a295ce04a8dd88f413e4b7
RDepeloProd <70080042+RDepeloProd@users.noreply.github.com>
2023-06-14 23:36:15 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -27,13 +27,19 @@ function adelgazarLetra(){
 var tituloState= document.getElementById("nameEstado");
 var fondo = document.getElementById("road");
 var descri = document.getElementById("text");
+var tp= document.getElementById("tp");
+var kms=document.getElementById("caracteristicas");
+
+
 
 function illinois(){
     tituloState.innerHTML = 'Illinois, ILL';
     descri.innerHTML= "En Illinois, la Ruta 66 comienza en Chicago y atraviesa ciudades históricas como Joliet y Springfield, ofreciendo una mezcla de paisajes urbanos y rurales."
-    fondo.style.backgroundColor = "#4DD19F"
+    fondo.style.backgroundColor = "#4DD19F";
     adelgazarLetra()
-    document.getElementById("il").style.fontWeight= 700
+    document.getElementById("il").style.fontWeight= 900
+    tp.href="estadosHTML/illinois.html";
+    kms.innerHTML='<br> Kms en la ruta: <b>503 km</b><br>Clima: <b>Inviernos fríos y veranos cálidos</b><br> Paisaje: <b>Paisajes rurales, granjas y pequeñas ciudades</b><br>'
 }
 function misuri(){
     tituloState.innerHTML = "Missouri, MO"
@@ -41,48 +47,68 @@ function misuri(){
     fondo.style.backgroundColor = "#74D09F"
     adelgazarLetra()
     document.getElementById("mo").style.fontWeight= 700
+    tp.href="estadosHTML/misuri.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>576 km</b><br>Clima: <b>Clima continental húmedo</b><br> Paisaje: <b>Colinas, bosques y valles y con pequeñas ciudades</b><br>'
+
 }
 function kansas(){
     tituloState.innerHTML = "Kansas, KS"
     descri.innerHTML= " El tramo de Kansas es corto pero pintoresco, con pequeñas ciudades y praderas que representan el paisaje de la América rural."
     fondo.style.backgroundColor = "#7FBD80"
     adelgazarLetra()
-    document.getElementById("ks").style.fontWeight= 700
+    document.getElementById("ks").style.fontWeight= 800
+    tp.href="estadosHTML/kansas.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>21 km</b><br>Clima: <b>Clima continental</b><br> Paisaje: <b>Áreas rurales y paisajes abiertos</b><br>'
+
 }
 function okla(){
     tituloState.innerHTML = "Oklahoma, OK"
     descri.innerHTML= "Oklahoma se destaca por sus extensas llanuras y ciudades como Tulsa y Oklahoma City, donde puedes explorar museos y monumentos relacionados con la Ruta 66."
     fondo.style.backgroundColor = "#BFBC69"
     adelgazarLetra()
-    document.getElementById("ok").style.fontWeight= 700
+    document.getElementById("ok").style.fontWeight= 800
+    tp.href="estadosHTML/okla.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>607 km</b><br>Clima: <b>Clima subtropical húmedo</b><br> Paisaje: <b>Desde colinas ondulantes hasta llanuras y desiertos</b><br>'
 }
 function texas(){
     tituloState.innerHTML = "Texas, TX"
     descri.innerHTML= "En Texas, la Ruta 66 atraviesa el Panhandle, ofreciendo paisajes desérticos y atracciones como el Cadillac Ranch y el MidPoint Café en Adrian."
     fondo.style.backgroundColor = "#FBD294"
     adelgazarLetra()
-    document.getElementById("tx").style.fontWeight= 700
+    document.getElementById("tx").style.fontWeight= 800
+    tp.href="estadosHTML/texas.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>917 km</b><br>Clima: <b>Clima subtropical en el este y semiárido en el oeste</b><br> Paisaje: <b>Amplias praderas, llanuras, desiertos y áreas urbanas importantes</b><br>'
+
 }
 function newMexico(){
     tituloState.innerHTML = "New Mexico, NM"
     descri.innerHTML= " Nuevo México ofrece una hermosa combinación de paisajes desérticos y montañosos, con paradas notables en Albuquerque, Santa Fe y el famoso pueblo de Gallup."
     fondo.style.backgroundColor = "#F9C5A8"
     adelgazarLetra()
-    document.getElementById("nm").style.fontWeight= 700
+    document.getElementById("nm").style.fontWeight= 800
+    tp.href="estadosHTML/newM.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>604 km</b><br>Clima: <b>Clima desértico en el sur hasta un clima semiárido en el norte</b><br> Paisaje: <b>Paisajes desérticos, mesetas, montañas y pueblos históricos de adobe</b><br>'
+
 }
 function arizona(){
     tituloState.innerHTML = "Arizona, AZ"
     descri.innerHTML= " En Arizona, puedes disfrutar del Gran Cañón, el pintoresco pueblo de Seligman y los paisajes desérticos que rodean Flagstaff y Kingman."
     fondo.style.backgroundColor = "#FCDAB1"
     adelgazarLetra()
-    document.getElementById("az").style.fontWeight= 700
+    document.getElementById("az").style.fontWeight= 800
+    tp.href="estadosHTML/arizona.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>401 km</b><br>Clima: <b>Clima desértico</b><br> Paisaje: <b>Paisajes desérticos, como el Gran Cañón, mesetas rocosas y formaciones geológicas únicas</b><br>'
+
 }
 function cali(){
     tituloState.innerHTML = "California, CA"
-    descri.innerHTML= " El último estado en la ruta, California, cuenta con destinos emblemáticos como Barstow, San Bernardino y, finalmente, Santa Mónica, donde la Ruta 66 llega a su fin en las hermosas playas del Pacífico."
+    descri.innerHTML= " El último estado en la ruta, California, cuenta con destinos emblemáticos como Barstow, San Bernardino y, finalmente, Santa Mónica, donde la Ruta 66 llega a su fin en las hermosas playas del Pacífico"
     fondo.style.backgroundColor = "#FCDAB1"
     adelgazarLetra()
-    document.getElementById("ca").style.fontWeight= 700
+    document.getElementById("ca").style.fontWeight= 800
+    tp.href="estadosHTML/cali.html"
+    kms.innerHTML='<br> Kms en la ruta: <b>490 km</b><br>Clima: <b>climas desérticos en el este hasta mediterráneos en la costa</b><br> Paisaje: <b>Una diversidad paisajística notable</b><br>'
+
 }
 
 
@@ -114,6 +140,7 @@ gsap.from(".dTexto",{
 gsap.from(".population",{
     scrollTrigger:{
     trigger: ".population",
+    markers:false,
     scrub:true,
     start:"top+=200 center",
     end:"bottom-=400 center",
--- code2.js
+++ code2.js
@@ -2,10 +2,32 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+function roadUno(){
+    document.getElementById("uno").style.opacity=1;
+}
+function roadDos(){
+    document.getElementById("dos").style.opacity=1;
+}
+function roadTres(){
+    document.getElementById("tres").style.opacity=1;
+}
+function roadCuatro(){
+    document.getElementById("cuatro").style.opacity=1;
+}
+function roadCinco(){
+    document.getElementById("cinco").style.opacity=1;
+}
+function roadSeis(){
+    document.getElementById("seis").style.opacity=1;
+}
+
+
 gsap.from(".dTitulo",{
     scrollTrigger:{
         trigger: "#detalles",
         scrub:true,
+        markers:false,
         start:"top-=100 center",
         end:"bottom-=800 center",
     },
@@ -16,6 +38,7 @@ gsap.from(".dTexto",{
     scrollTrigger:{
         trigger: "#detalles",
         scrub:true,
+        markers:false,
         start:"top-=50 center",
         end:"bottom-=750 center",
     },
@@ -26,6 +49,7 @@ gsap.from(".dTexto",{
 gsap.from(".population",{
     scrollTrigger:{
     trigger: ".population",
+    markers:false,
     scrub:true,
     start:"top+=200 center",
     end:"bottom-=400 center",
@@ -33,3 +57,93 @@ gsap.from(".population",{
 y:-400,
 opacity:0
 })
+
+gsap.from(".foto img",{
+    scrollTrigger:{
+    trigger: ".foto",
+    markers:false,
+    scrub:true,
+    start:"top+=200 center",
+    end:"bottom-=400 center",
+},
+x:-400,
+opacity:0
+})
+
+gsap.from(".foto h1",{
+    scrollTrigger:{
+    trigger: ".foto",
+    markers:false,
+    scrub:true,
+    start:"top+=200 center",
+    end:"bottom-=400 center",
+},
+x:400,
+opacity:0
+})
+
+gsap.from(".needles",{
+    scrollTrigger:{
+    trigger: ".publi",
+    markers:false,
+    scrub:true,
+    start:"top+=100 center",
+    end:"bottom-=1400 center",
+},
+x:-400,
+opacity:0,
+onUpdate: roadUno
+})
+
+gsap.from(".calico",{
+    scrollTrigger:{
+    trigger: ".calico",
+    markers:false,
+    scrub:true,
+    start:"top+=100 center",
+    end:"bottom-=1300 center",
+},
+x:-400,
+opacity:0,
+onUpdate: roadDos
+})
+
+gsap.from(".bottle",{
+    scrollTrigger:{
+    trigger: ".bottle",
+    markers:false,
+    scrub:true,
+    start:"top+=200 center",
+    end:"bottom-=1250 center",
+},
+x:-400,
+opacity:0,
+onUpdate: roadTres
+})
+
+gsap.from(".wigman",{
+    scrollTrigger:{
+    trigger: ".wigman",
+    markers:false,
+    scrub:true,
+    start:"top+=200 center",
+    end:"bottom-=1400 center",
+},
+x:-400,
+opacity:0,
+onUpdate: roadCuatro
+})
+
+gsap.from(".la",{
+    scrollTrigger:{
+    trigger: ".la",
+    markers:false,
+    scrub:true,
+    start:"top+=200 center",
+    end:"bottom-=1950 center",
+},
+x:-400,
+opacity:0,
+onUpdate: roadCinco
+})
+
